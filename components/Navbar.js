b1c8a00696d2e0b1d22b08f1c3042848ba2bdc7d
Ram Murmu <runashInc@outlook.com>
2022-10-15 20:58:11 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <main className="flex w-full flex-1 flex-col item-centre justify-centre px-20 text-centre">
+        <main className="flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
             <div className="flex h-20 w-full items-center justify-center">
                 <text className="text-1xl font-normal text-black mt-1 w-60 h-6">
                     <p className="text-1xl font-normal">Welcome to RunAsh &rarr;</p>
@@ -19,7 +19,7 @@ export default function Navbar() {
             <h1 className="text-2xl font-b">RunAsh</h1>
         </div>
         <div className="flex flex-row item-centre space-x-4">
-            <ul className="flex flex-row item-centre">
+            <ul className="mt-10 pb-8 px-[50vm] w-full flex overflo-x-auto gap-8 snap-x flex-row item-centre">
                 <li className="mr-10">
                     <Link href="/live">
                         <a className="text-lg font-n px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Live</a>

306eac0f7f4da1e100d9c291270a1cfad0f1206f
Ram Murmu <runashInc@outlook.com>
2022-10-16 17:33:20 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -244,13 +244,13 @@ export default function Index() {
              <a>Give your most valuable feedback to improve live experience better</a>
           </Link>
           </p>
-        <div className="mt-12 grid grid-flow-col">
+        <div className="mt-2 grid grid-flow-col">
           <button className="bg-white-600 text-gray-400 hover:bg-amber-50 hover:text-black font-n py-1 px-3 rounded mt-0">😭</button>
           <button className="bg-white-600 text-gray-400 hover:bg-amber-50 hover:text-black font-n py-1 px-3 rounded mt-0">😕</button>
           <button className="bg-white-600 text-gray-400 hover:bg-amber-50 hover:text-black font-n py-1 px-3 rounded mt-0">😃</button>
           <button className="bg-white-600 text-gray-400 hover:bg-amber-50 hover:text-black font-n py-1 px-3 rounded mt-0">🤩</button>
          </div>
-          <button className="mt-12 mr-12 bg-blue-600 text-white hover:bg-amber-50 hover:text-black font-n py-1 px-3 rounded mt-0">feedback</button>
+          <button className="mt-12 bg-blue-600 text-white hover:bg-amber-50 hover:text-black font-n py-1 px-3 rounded mt-0">feedback</button>
         
 
         <div className="mt-8 flex max-w-8xl flex-wrap item-centre justify-around sm:w-full bg-amber-50 p-8 rounded">

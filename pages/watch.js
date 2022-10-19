2c25f320aae6b33d3aab0d668f31a6233bb875a8
Ram Murmu <runashInc@outlook.com>
2022-10-19 20:49:51 +0530

    Update watch.js

--- pages/watch.js
+++ pages/watch.js
@@ -5,12 +5,75 @@ import Image from 'next/image'
 export default function Watch() {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
-            <h1 className="text-6xl font-n">
-                Watch live stream
+            <h1 className="text-6xl font-n text-left">
+                Live on demand
             </h1>
-            <p className="mt-6">
+            <p className="mt-6 text-left">
                 Watch previous live on demand streaming
             </p>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
+            <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
+                <Image className="inline" src="/" alt=""></Image>
+                <h2 className="text-2xl font-n">
+                 Organic fruit
+                </h2>
+                <p className="mt-6">
+                   Fresh
+                </p>
+            </div>
             <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
                 <Image className="inline" src="/" alt=""></Image>
                 <h2 className="text-2xl font-n">

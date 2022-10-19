23ca14ab9c10f438fb2e3868f0654d3d8622b0b1
Ram Murmu <runashInc@outlook.com>
2022-10-19 21:06:38 +0530

    create dashboard.js

---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Dashboard() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <h1 className="text-6xl bg-gray-50 font-n">
+                Welcome to dashboard
+            </h1>
+        </div>
+        )
+    }

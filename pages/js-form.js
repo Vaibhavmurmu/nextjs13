1e71c846da8b2ee7a4c4dedd9e90102ca81d1233
Ram Murmu <runashInc@outlook.com>
2022-10-19 23:38:06 +0530

    Change to be committed

--- pages/js-form.js
+++ pages/js-form.js
@@ -49,118 +49,17 @@ export default function PageWithJSbasedForm() {
       <div className="mt-6 w-96 rounded-xl border p-6 text-left focus:text-blue-600">
       <p className="mt-12 text-2xl bottom-2">Create your account ID</p>
       <form onSubmit={handleSubmit}>
-        <div class="py-12">
-          <h2 class="text-2xl font-bold">Simple</h2>
-          <div class="mt-8 max-w-md">
-            <div class="grid grid-cols-1 gap-6">
-              <label class="block">
-                <span class="text-gray-700">Full name</span>
-                <input
-                  type="text"
-                  class="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                  placeholder=""
-                />
-              </label>
-              <label class="block">
-                <span class="text-gray-700">Email address</span>
-                <input
-                  type="email"
-                  class="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                  placeholder="john@example.com"
-                />
-              </label>
-              <label class="block">
-                <span class="text-gray-700">When is your event?</span>
-                <input
-                  type="date"
-                  class="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                />
-              </label>
-              <label class="block">
-                <span class="text-gray-700">What type of event is it?</span>
-                <select
-                  class="
-                    block
-                    w-full
-                    mt-1
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                >
-                  <option>Corporate event</option>
-                  <option>Wedding</option>
-                  <option>Birthday</option>
-                  <option>Other</option>
-                </select>
-              </label>
-              <label class="block">
-                <span class="text-gray-700">Additional details</span>
-                <textarea
-                  class="
-                    mt-1
-                    block
-                    w-full
-                    rounded-md
-                    border-gray-300
-                    shadow-sm
-                    focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                  "
-                  rows="3"
-                ></textarea>
-              </label>
-              <div class="block">
-                <div class="mt-2">
-                  <div>
-                    <label class="inline-flex items-center">
-                      <input
-                        type="checkbox"
-                        class="
-                          rounded
-                          border-gray-300
-                          text-indigo-600
-                          shadow-sm
-                          focus:border-indigo-300
-                          focus:ring
-                          focus:ring-offset-0
-                          focus:ring-indigo-200
-                          focus:ring-opacity-50
-                        "
-                        checked
-                      />
-                      <span class="ml-2">Email me news and special offers</span>
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <label htmlFor="email">email</label>
+        <input type="text" id="email" name="email" required />
+        <label htmlFor="fast">Fast Name</label>
+        <input type="text" id="first" name="first" required />
+        <label htmlFor="last">Last Name</label>
+        <input type="text" id="last" name="last" required />
+        <label htmlFor="Phone number">Phone number</label>
+        <input type="text" id="Phone number" name="Optional" required />
+        <label htmlFor="password">Password</label>
+        <input type="text" id="password" name="password" required />
+        <button type="sign up">Sign Up</button>
       </form>
       
       <p className="mt-12 text-0xl bottom-2">

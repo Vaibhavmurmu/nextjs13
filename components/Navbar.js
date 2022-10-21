b8c6ac9ac47d9153676e9a4aa08925343069e62b
Ram Murmu <runashInc@outlook.com>
2022-10-21 19:14:14 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,12 +9,12 @@ export default function Navbar() {
                     <p className="text-1xl font-normal">Welcome to RunAsh &rarr;</p>
                 </text>
             </div>
-        <div className="flex flex-row items-center justify-between w-full px-6 py-4">
+        <div className="flex flex-row items-center justify-center w-full border-t h-4 px-6 py-4">
         <div className="flex flex-row items-center">
             <h1 className="text-2xl font-b">RunAsh</h1>
         </div>
         <div className="flex flex-row item-center space-x-4">
-            <ul className="flex flex-row item-center">
+            <ul className="mr-12 flex flex-row item-center">
                 <li className="mr-12">
                     <Link href="/live">
                         <a className="text-lg font-n px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Live</a>

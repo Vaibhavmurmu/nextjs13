d7e6caccc9536a916117074e228619303f951d7b
Ram Murmu <runashInc@outlook.com>
2022-10-21 07:35:19 +0000

    Change to be committed

--- components/Footer.js
+++ components/Footer.js
@@ -40,7 +40,7 @@ export default function Footer() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    © 2022 RunAsh{' '} All rights reserved
+                    © 2022 RunAsh.{' '} All rights reserved
                 </a>
             </div>
         </div>

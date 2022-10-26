0acdef235bf5a7b4eff895c61f06473327902911
Ram Murmu <runashInc@outlook.com>
2022-10-26 15:08:49 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,6 @@
 import Link from "next/link"
 import Image from "next/image"
-import React {useState} from "react"
+import React { useState } from "react"
 import { Bars2Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 export default function Navbar() {

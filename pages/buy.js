600a77a59688d2d641a5567e286299877e6eace3
Ram Murmu <runashInc@outlook.com>
2022-10-29 12:56:17 +0000

    Change to be committed

--- pages/buy.js
+++ pages/buy.js
@@ -13,33 +13,33 @@ export default function Sell() {
       <main className="flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
         <card className="mt-12 w-100 rounded-xl border p-3 text-left hover:text-blue-600 focus:text-blue-600">
           <text className="text-1xl font-normal mt-2">
-            <p className="text-1xl font-normal"><button className="bg-blue-600 text-white font-n py-1 px-3 rounded mt-0">Latest</button> Introducing next generation and futre of retail &rarr;</p>   
+            <p className="text-1xl font-normal"><button className="bg-blue-600 text-white font-n py-1 px-3 rounded mt-0">Latest</button> Introducing next generation and futre of shopping &rarr;</p>   
           </text>
           </card> 
         <h1 className="mt-12 text-6xl font-bold">
           NextGen shopping{' '}<br /> 
           <a className="text-orange-600" href="https://nextjs.org">
-           experience.
+           experience
           </a>
         </h1>
 
         <p className="mt-12 text-2xl bottom-2">
-        You can meet to seller with face-to-face real life experience.
+        You can meet to seller with face-to-face real life experience
         </p>
-        <div className="flex gap-6 mt-6 max-w-0xl flex-wrap items-center justify-around">
-        <button className="bg-orange-600 hover:bg-green-600 text-white font-bold py-3 px-8 rounded mt-12">
+        <div className="flex gap-6 mt-2 max-w-0xl flex-wrap items-center justify-around">
+        <button className="bg-orange-600 hover:bg-green-600 text-white font-bold py-3 px-14 rounded mt-4">
           <Link href="/login">
             <a>Start shopping</a>
           </Link>
         </button>
-        <button className="bg-orange-600 hover:bg-green-600 text-white font-bold py-3 px-8 rounded mt-12">
+        <button className="bg-orange-600 hover:bg-green-600 text-white font-bold py-3 px-14 rounded mt-4">
           <Link href="/register">
             <a>Watch products</a>
           </Link>
         </button>
         </div>
             <p className='mt-8 text-2xl font-normal'>
-                Unique shopping experience with live streaming.
+                Unique real experience 
             </p>
             <div className="mt-6 flex max-w-7xl flex-wrap items-center justify-around sm:w-full">
           <a
@@ -58,7 +58,7 @@ export default function Sell() {
             >
             <h3 className="text-2xl font-bold">Global reach</h3>
             <p className="mt-4 text-xl">
-               Reaches your organic product need,around the worlds future organic products 
+               Reaches your organic product need,around the worlds 
             </p>
             </a>
 
@@ -128,7 +128,7 @@ export default function Sell() {
             >
             <h3 className="text-2xl font-bold">DiffServe</h3>
             <p className="mt-4 text-xl">
-                The promise of high quality organic products
+                The promise of high quality organic products and experice
             </p>
             </a>
 
@@ -163,37 +163,37 @@ export default function Sell() {
             </a>
         </div>
         <h2 className='mt-24 text-2xl font-normal'>
-            Unique advantage for live sell
+            Advantage for live shopping
         </h2>
-        <p className="mt-6">
-          Your business and sales experience with earning more benifits.
+        <p className="mt-6 text-xl">
+          Real shopping and barganing experience
         </p>
             <div className="mt-6 flex max-w-7xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">1st payment</h3>
+            <h3 className="text-2xl font-bold">Secure payment</h3>
             <p className="mt-4 text-xl">
-                Real time disvered your payment
+                Payment gatway is secured and safe
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Trusted</h3>
+            <h3 className="text-2xl font-bold">Trusted product</h3>
             <p className="mt-4 text-xl">
-                Trusted payment system
+                Trusted organic products and services
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Secure</h3>
+            <h3 className="text-2xl font-bold">Secure transaction</h3>
             <p className="mt-4 text-xl">
-                Your conten is safe and secure
+                Your shopping data is safe and secure
             </p>
           </a>
           <a
@@ -202,7 +202,7 @@ export default function Sell() {
           >
             <h3 className="text-2xl font-bold">Verified</h3>
             <p className="mt-4 text-xl">
-                Your products will be verified by us
+                Organic products is runash verified
             </p>
           </a>
           <a
@@ -211,43 +211,70 @@ export default function Sell() {
           >
             <h3 className="text-2xl font-bold">Private</h3>
             <p className="mt-4 text-xl">
-                Your live streaming is private
+                Your live streaming content is private
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Organic</h3>
+            <h3 className="text-2xl font-bold">Organic product</h3>
             <p className="mt-4 text-xl">
-                Organic business,product and brand
+                Organic and fresh products
+            </p>
+          </a>
+          <a
+            href="https://nextjs.org/docs"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Fast delivery</h3>
+            <p className="mt-4 text-xl">
+                Fast delivery in your door step
+            </p>
+          </a>
+          <a
+            href="https://nextjs.org/docs"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Easy return</h3>
+            <p className="mt-4 text-xl">
+                Easy return of organic products
+            </p>
+          </a>
+          <a
+            href="https://nextjs.org/docs"
+            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
+          >
+            <h3 className="text-2xl font-bold">Pay on delivery</h3>
+            <p className="mt-4 text-xl">
+                Products your hands before you pay
             </p>
           </a>
           </div>
           <h2 className='mt-12 text-3xl font-b'>
             How it works
           </h2>
-          <p className="mt-6">
-           Get started in few steps and go live streaming
+          <p className="mt-6 text-xl">
+           Few steps and get started live shopping
           </p>
-          <div className="mt-8 flex  max-w-10xl flex-wrap items-center justify-around sm:w-full">
+          <div className="mt-8 flex  max-w-7xl flex-wrap items-center justify-around sm:w-full">
           <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">1</button> Create your live account
           </p>
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">2</button> Record and upload content
+          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">2</button> Watch and schedule a live streaming 
           </p>
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">3</button> Set prefered date and time
+          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">3</button> schedule prefered date and time
           </p>
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">4</button> Get ready for live-stream
+          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">4</button> Get ready for join live-stream
           </p>
           </div>
           <h2 className='mt-24 text-3xl font-b'>
-            The Live Demonstration
+            Real experience
             </h2>
-          <p className="mt-6">
-           Our team personally explains you, how to go live 
+          <p className="mt-6 text-xl">
+           Watched and schedule live streaming
           </p>
           <div className="mt-8 flex max-w-full flex-wrap item-centre justify-around bg-gray-50 sm:w-full p-8 rounded">
-          <h2 className="mt-2 text-bold">
+          <h2 className="mt-2 text-xl">
             Get live stream updates
           </h2>
           <input className="mt-2 focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-48 text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Email address" placeholder="Email address..."></input>
--- pages/watch.js
+++ pages/watch.js
@@ -11,44 +11,42 @@ export default function Sell() {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
-        <card className="mt-12 w-100 rounded-xl border p-3 text-left hover:text-blue-600 focus:text-blue-600">
-          <text className="text-1xl font-normal mt-2">
-            <p className="text-1xl font-normal"><button className="bg-blue-600 text-white font-n py-1 px-3 rounded mt-0">Latest</button> Introducing next generation and futre of retail &rarr;</p>   
-          </text>
-          </card> 
-        <h1 className="mt-12 text-6xl font-bold">
-          Live on demand{' '}<br /> 
-          <a className="text-orange-600" href="https://nextjs.org">
-           streaming.
+        <h1 className="mt-14 text-6xl font-bold">
+          Live-on demand{' '}<br /> 
+          <a className="text-black" href="https://nextjs.org">
           </a>
         </h1>
 
-        <p className="mt-12 text-2xl bottom-2">
-        Watch previous live recording streaming.
+        <p className="mt-6 text-2xl bottom-2 text-gray-150">
+        Watch previous live streams and get notified when we go live again.
         </p>
-        <div className="flex gap-6 mt-6 max-w-0xl flex-wrap items-center justify-around">
-        <button className="bg-orange-600 hover:bg-green-600 text-white font-bold py-3 px-8 rounded mt-12">
-          <Link href="/login">
-            <a>Start building</a>
-          </Link>
-        </button>
-        <button className="bg-orange-600 hover:bg-green-600 text-white font-bold py-3 px-8 rounded mt-12">
-          <Link href="/register">
-            <a>Get a demo</a>
-          </Link>
-        </button>
-        </div>
-            <p className='mt-8 text-2xl font-normal'>
-                Unique features
-            </p>
+        <div className="flex gap-6 mt-2 max-w-0xl flex-wrap items-left justify-around">
+        <label class="grid">
+                <span className="text-black text-left">Categeries</span>
+                <input className="grid w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" type="text" aria-label="Search" placeholder="Search..."></input>
+              </label>
+            
+            </div>
+            <div className="flex gap-6 mt-2 max-w-0xl flex-wrap items-center justify-around">
+            <label className="grid">
+                <span className="text-gray-700">All categeries</span>
+                <select className="grid w-full mt-1 rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+                  <option>Select categeries</option>
+                  <option>Fruit</option>
+                  <option>Dairy food</option>
+                  <option>Dray food</option>
+                </select>
+              </label>
+            </div>
+            
             <div className="mt-6 flex max-w-7xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Ubiquity</h3>
+            <h3 className="text-2xl font-bold">Vegetables</h3>
             <p className="mt-4 text-xl">
-               Reduce your selling costs and sustain more efficient
+               Buy fresh vegetables from local farmers.
             </p>
           </a>
 
@@ -56,9 +54,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Global reach</h3>
+            <h3 className="text-2xl font-bold">Fruits</h3>
             <p className="mt-4 text-xl">
-               Reaches your customers,around the earth 
+              Buy fresh organic fruits from local farmers. 
             </p>
             </a>
 
@@ -66,9 +64,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Universal standards</h3>
+            <h3 className="text-2xl font-bold">Dairy food</h3>
             <p className="mt-4 text-xl">
-               High quality live streaming technology across the globe
+              Buy fresh dairy food from local farmers.
             </p>
             </a>
 
@@ -76,9 +74,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Richness</h3>
+            <h3 className="text-2xl font-bold">Spices</h3>
             <p className="mt-4 text-xl">
-              Real video selling are possible and cusuming real experience
+              Buy fresh spices from local farmers.
             </p>
             </a>
 
@@ -86,9 +84,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Interactivity</h3>
+            <h3 className="text-2xl font-bold">Dray food</h3>
             <p className="mt-4 text-xl">
-              Engaged face to face interaction with customers 
+              Buy fresh dry food from local farmers.
             </p>
             </a>
 
@@ -96,9 +94,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Information density</h3>
+            <h3 className="text-2xl font-bold">Flower</h3>
             <p className="mt-4 text-xl">
-              Reduces costs and raises high quality business 
+              Buy fresh flowers from local farmers.
             </p>
             </a>
 
@@ -106,9 +104,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Personalization</h3>
+            <h3 className="text-2xl font-bold">Sea food</h3>
             <p className="mt-4 text-xl">
-               Technology allows personalized own made business experience
+              Buy fresh sea food from local farmers.
             </p>
             </a>
 
@@ -116,9 +114,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Customisation</h3>
+            <h3 className="text-2xl font-bold">Meat</h3>
             <p className="mt-4 text-xl">
-               Technology delivered real experience on business characteristics
+              Buy fresh meat from local farmers.
             </p>
             </a>
 
@@ -126,9 +124,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">DiffServe</h3>
+            <h3 className="text-2xl font-bold">Oil</h3>
             <p className="mt-4 text-xl">
-                The promise of high quality live streaming
+              Buy fresh oil from local farmers.
             </p>
             </a>
 
@@ -136,9 +134,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Physcial reality</h3>
+            <h3 className="text-2xl font-bold">Grocery</h3>
             <p className="mt-4 text-xl">
-                Real selling experice on real digitization
+              Buy fresh grocery from local farmers.
             </p>
             </a>
 
@@ -146,9 +144,9 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Light/dark theme</h3>
+            <h3 className="text-2xl font-bold">Agri product</h3>
             <p className="mt-4 text-xl">
-               Light and dark protanopia and deuterannopia theme 
+              Buy fresh agri product from local farmers. 
             </p>
             </a>
 
@@ -156,103 +154,77 @@ export default function Sell() {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
             >
-            <h3 className="text-2xl font-bold">Mobile fast</h3>
+            <h3 className="text-2xl font-bold">Handicraft</h3>
             <p className="mt-4 text-xl">
-                High performance real streaming on mobile devices
+              Buy fresh handicraft from local farmers.
             </p>
             </a>
         </div>
-        <h2 className='mt-24 text-2xl font-normal'>
-            Unique advantage for live sell
+        <h2 className='mt-24 text-2xl font-normal text-left'>
+            Organic products
         </h2>
-        <p className="mt-6">
-          Your business and sales experience with earning more benifits.
-        </p>
             <div className="mt-6 flex max-w-7xl flex-wrap items-center justify-around sm:w-full">
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">1st payment</h3>
+            <h3 className="text-2xl font-bold">Apple</h3>
             <p className="mt-4 text-xl">
-                Real time disvered your payment
+              Buy fresh apple from local farmers.
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Trusted</h3>
+            <h3 className="text-2xl font-bold">Banana</h3>
             <p className="mt-4 text-xl">
-                Trusted payment system
+              Buy fresh banana from local farmers.
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Secure</h3>
+            <h3 className="text-2xl font-bold">Grapes</h3>
             <p className="mt-4 text-xl">
-                Your conten is safe and secure
+              Buy fresh grapes from local farmers.
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Verified</h3>
+            <h3 className="text-2xl font-bold">Orange</h3>
             <p className="mt-4 text-xl">
-                Your products will be verified by us
+              Buy fresh orange from local farmers.
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Private</h3>
+            <h3 className="text-2xl font-bold">Blackberry</h3>
             <p className="mt-4 text-xl">
-                Your live streaming is private
+              Buy fresh blackberry from local farmers.
             </p>
           </a>
           <a
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Organic</h3>
+            <h3 className="text-2xl font-bold">Pineapple</h3>
             <p className="mt-4 text-xl">
-                Organic business,product and brand
+              Buy fresh pineapple from local farmers.
             </p>
           </a>
           </div>
           <h2 className='mt-12 text-3xl font-b'>
-            How it works
+            
           </h2>
           <p className="mt-6">
-           Get started in few steps and go live streaming
+          
           </p>
-          <div className="mt-8 flex  max-w-10xl flex-wrap items-center justify-around sm:w-full">
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">1</button> Create your live account
-          </p>
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">2</button> Record and upload content
-          </p>
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">3</button> Set prefered date and time
-          </p>
-          <p className='mt-6 w-96 rounded-xl border p-6 text-left bg-white hover:text-blue-600 focus:text-blue-600'><button className="bg-black text-white font-n py-0 px-2 rounded-3xl mt-0">4</button> Get ready for live-stream
-          </p>
-          </div>
-          <h2 className='mt-24 text-3xl font-b'>
-            The Live Demonstration
-            </h2>
-          <p className="mt-6">
-           Our team personally explains you, how to go live 
-          </p>
-          <div className="mt-8 flex max-w-full flex-wrap item-centre justify-around bg-gray-50 sm:w-full p-8 rounded">
-          <h2 className="mt-2 text-bold">
-            Get live stream updates
-          </h2>
-          <input className="mt-2 focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-48 text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Email address" placeholder="Email address..."></input>
-          <button className="mt-2 bg-black text-white hover:bg-white hover:text-black font-n py-1 px-3 rounded">Subscribe</button>
-        </div>
         </main>
         </div>
         )

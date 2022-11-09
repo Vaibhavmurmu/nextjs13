314f364908214e1b395676ae6b6c686d0b75649b
Ram Murmu <runashInc@outlook.com>
2022-11-09 14:57:13 +0000

    Change to be committed

---
+++ components/Dropdown.js
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import { Menu } from '@headlessui/react'
+
+export default function Dropdown() {
+    return (
+        <div className="">
+      <Menu>
+        <Menu.Button>More</Menu.Button>
+        <Menu.Items>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${active && 'bg-blue-500 mt-1'}`}
+                href="/account-settings"
+              >
+                Account settings
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item>
+            {({ active }) => (
+              <a
+                className={`${active && 'bg-blue-500 mt-1'}`}
+                href="/account-settings"
+              >
+                Documentation
+              </a>
+            )}
+          </Menu.Item>
+          <Menu.Item disabled>
+            <span className="opacity-75 mt-1">Invite(coming soon!)</span>
+          </Menu.Item>
+        </Menu.Items>
+      </Menu>
+    </div>
+    )
+  }
+  

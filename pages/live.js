2b946648ff32849e3808159319cff17c3985fea6
Ram Murmu <runashInc@outlook.com>
2022-11-09 14:56:35 +0000

    Change to be committed

--- pages/live.js
+++ pages/live.js
@@ -1,15 +1,35 @@
 import Link from 'next/link'
 import Scroll from '../components/Scroll'
+import Dropdown from '../components/Dropdown'
+import Menu from '../components/Menu'
+import Switch from '../components/Switch'
+
 
 
 
 export default function Live() {
     return (
-        <div className="flex min-h-screen flex-col items-center justify-center py-2">
-            <h1 className="text-6xl font-n">
-                Creat your live stream
-            </h1>
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
             <Scroll />
+            </div>
+
+            <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+            <Dropdown />
+            </div>
+
+            <div className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+            <Menu />
+            </div>
+
+            <div className="mt-16 ">
+            <Switch />
+            </div>
+
+            <div className="">
+            
+            </div>
         </div>
+
         )
     }

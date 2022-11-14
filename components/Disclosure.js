dd640700e39df3c301e2182f64e68da36a0dfdcf
Ram Murmu <runashInc@outlook.com>
2022-11-14 18:17:03 +0530

    Update Disclosure.js

--- components/Disclosure.js
+++ components/Disclosure.js
@@ -18,8 +18,8 @@ export default function Example() {
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+                If you are unhappy with your purchase for any reason email us
+                within 90 days and well refund you in full no questions asked.
               </Disclosure.Panel>
             </>
           )}
@@ -39,8 +39,8 @@ export default function Example() {
                 />
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
+                If you are unhappy with your purchase for any reason email us
+                within 90 days and well refund you in full no questions asked.
               </Disclosure.Panel>
             </>
           )}

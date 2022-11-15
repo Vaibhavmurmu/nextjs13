3258b1a36b409179530a8686b0b624982715435f
Ram Murmu <runashInc@outlook.com>
2022-11-15 14:27:09 +0530

    Update Dropdown.js

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -6,14 +6,15 @@ export default function Dropdown() {
         <div className="top-8 w-56 text-right">
       <Menu as="div" className="relative inline-block text-left">
         <Menu.Button className="inline-flex w-full justify-centre rounded-md bg-white bg-opacity-20 px-2 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">More</Menu.Button>
-        <Menu.Items>
-          <Menu.Item>
+        <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+         <div className="px-1 py-1"> 
+        <Menu.Item>
             {({ active }) => (
               <a
-                className={`${active && 'bg-blue-500 mt-1 flex flex-col'}`}
+                className={`${active && 'bg-blue-500 text-white text-gray-900  group flex w-full item-centre rounded-md px-2 py-2 text-sm mt-1 flex flex-col'}`}
                 href="/account-settings"
               >
-                Account settings
+                Account 
               </a>
             )}
           </Menu.Item>
@@ -23,12 +24,12 @@ export default function Dropdown() {
                 className={`${active && 'bg-blue-500 mt-1 flex flex-col'}`}
                 href="/account-settings"
               >
-                On site schedule
+                Schedule
               </a>
             )}
           </Menu.Item>
           <Menu.Item disabled>
-            <span className="opacity-75 mt-1 flex flex-col">Schedule</span>
+            <span className="opacity-75 mt-1 flex flex-col">Watch</span>
           </Menu.Item>
         </Menu.Items>
       </Menu>

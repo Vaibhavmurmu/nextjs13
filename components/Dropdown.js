6a468f28dac385cc97821dd656a9a997e33b2757
Ram Murmu <runashInc@outlook.com>
2022-11-15 14:17:08 +0530

    Update Dropdown.js

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -3,14 +3,14 @@ import { Menu } from '@headlessui/react'
 
 export default function Dropdown() {
     return (
-        <div className="flex flax-col">
-      <Menu>
-        <Menu.Button>More</Menu.Button>
+        <div className="top-8 w-56 text-right">
+      <Menu as="div" className="relative inline-block text-left">
+        <Menu.Button className="inline-flex w-full justify-centre rounded-md bg-white bg-opacity-20 px-2 py-2 text-sm font-medium text-black hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">More</Menu.Button>
         <Menu.Items>
           <Menu.Item>
             {({ active }) => (
               <a
-                className={`${active && 'bg-blue-500 mt-1'}`}
+                className={`${active && 'bg-blue-500 mt-1 flex flex-col'}`}
                 href="/account-settings"
               >
                 Account settings
@@ -20,7 +20,7 @@ export default function Dropdown() {
           <Menu.Item>
             {({ active }) => (
               <a
-                className={`${active && 'bg-blue-500 mt-1'}`}
+                className={`${active && 'bg-blue-500 mt-1 flex flex-col'}`}
                 href="/account-settings"
               >
                 On site schedule
@@ -28,7 +28,7 @@ export default function Dropdown() {
             )}
           </Menu.Item>
           <Menu.Item disabled>
-            <span className="opacity-75 mt-1">Schedule</span>
+            <span className="opacity-75 mt-1 flex flex-col">Schedule</span>
           </Menu.Item>
         </Menu.Items>
       </Menu>

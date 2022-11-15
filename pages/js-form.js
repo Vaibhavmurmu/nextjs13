6125c18ca42a59c3daeb0e31dae4f5ae14804035
Ram Murmu <runashInc@outlook.com>
2022-11-15 12:01:48 +0530

    Update js-form.js

--- pages/js-form.js
+++ pages/js-form.js
@@ -35,7 +35,7 @@ export default function PageWithJSbasedForm() {
   }
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-    <main className="flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
+    <main className="mt-16 flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
     
       <h1 className="mt-12 text-2xl font-bold">
         <Link href="/">

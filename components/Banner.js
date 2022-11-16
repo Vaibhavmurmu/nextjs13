06adbc178fd4dc62055d17a83c9e0fbdab372ab0
Ram Murmu <runashInc@outlook.com>
2022-11-16 17:26:29 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -25,7 +25,7 @@ export default function Example() {
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
               type="button"
-              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+              className="mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
               <span className="sr-only">Dismiss</span>
               <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
             </button>

d2eda5d383028157b5d0c359cab559a9952f3f3f
Ram Murmu <runashInc@outlook.com>
2022-11-16 15:35:23 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -1,8 +1,6 @@
 import Link from 'next/link'
-import Scroll from '../components/Scroll'
-import Dropdown from '../components/Dropdown'
-import Menu from '../components/Menu'
-import Switch from '../components/Switch'
+import Dropdown from '../components/Dashboard'
+
 
 
 
@@ -11,23 +9,7 @@ export default function Live() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <Scroll />
-            </div>
-
-            <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <Dropdown />
-            </div>
-
-            <div className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
-            <Menu />
-            </div>
-
-            <div className="mt-16 ">
-            <Switch />
-            </div>
-
-            <div className="">
-            
+            <Dashboard />
             </div>
         </div>
 

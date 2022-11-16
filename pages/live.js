3f28c44da8f01e3969443d0641d484399a55533f
Ram Murmu <runashInc@outlook.com>
2022-11-16 15:50:05 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import Dashboard from '../components/Dashboard'
+import Menunavbar from '../components/Menunavbar'
 
 export default function Live() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <Dashboard />
+            <Menunavbar />
             </div>
         </div>
 

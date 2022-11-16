b27bc4df2d372ed083186563bee64b88a33d7bf1
Ram Murmu <runashInc@outlook.com>
2022-11-16 16:04:59 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import Simpledropdown from '../components/Simpledropdown'
+import Signin from '../components/Signin'
 
 export default function Live() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <Simpledropdown />
+            <Signin />
             </div>
         </div>
 

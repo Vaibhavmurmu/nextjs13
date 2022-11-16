f541b1e90f740fdaebb53bea0416df14a6b418b8
Ram Murmu <runashInc@outlook.com>
2022-11-16 17:20:31 +0530

    Update Banner.js

--- components/Banner.js
+++ components/Banner.js
@@ -18,16 +18,14 @@ export default function Example() {
           <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
             <a
               href="#"
-              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50"
-            >
+              className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-2 text-sm font-medium text-indigo-600 shadow-sm hover:bg-indigo-50">
               Learn more
             </a>
           </div>
           <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
             <button
               type="button"
-              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
-            >
+              className="-mr-1 flex rounded-md p-2 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
               <span className="sr-only">Dismiss</span>
               <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
             </button>

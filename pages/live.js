430c5f0f875c84ceb1739370ea41ae7d638fb280
Ram Murmu <runashInc@outlook.com>
2022-11-16 15:56:45 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import Menunavbar from '../components/Menunavbar'
+import Simpledropdown from '../components/Simpledropdown'
 
 export default function Live() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-            <Menunavbar />
+            <Simpledropdown />
             </div>
         </div>
 

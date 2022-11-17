02aac00b7f3b119bd20ce6c558f2e76a464dce90
Ram Murmu <runashInc@outlook.com>
2022-11-17 14:38:47 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -7,16 +7,17 @@ export default function Live() {
         <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
          <div className="md:flex">
           <div className="md:shrink-0">
-            <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture">
+            <img className="h-48 w-full object-cover md:h-full md:w-48" src="/vercel.svg" alt="Modern building architecture">
         </div>
         <div className="p-8">
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
           <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
             <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
          </div>
-        </div>
        </div>
-       </div>
-       </div>
-    )
- }
+     </div>
+    </div>
+   </div>
+ )
+ 
+}

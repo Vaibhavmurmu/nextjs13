8c5ab296e347db5c5d229cc1d43f350878b77d61
Ram Murmu <runashInc@outlook.com>
2022-11-17 18:08:05 +0530

    Update login.jsx

--- pages/login.jsx
+++ pages/login.jsx
@@ -18,15 +18,15 @@ if (session) {
     );
 }
 return (
-    <main className="mt-6 flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
-    <div className="mt-6 flex min-h-screen flex-col items-center justify-center py-2">
-      <h1 className="mt-4 text-2xl font-bold">
+    <main className="mt-0 flex w-full flex-1 flex-col items-center justify-centre px-20 text-center">
+    <div className="mt-0 flex min-h-screen flex-col items-center justify-center py-2">
+      <h1 className="mt-2 text-2xl font-bold">
         <Link href="/">
           <a>Sign in to</a>
         </Link>{' '}RunAsh 
       </h1>
       
-      <p className="mt-10 text-1xl bottom-2">
+      <p className="mt-4 text-1xl bottom-2">
         Lets being the live streaming journey
       </p>
       <div className="mt-6 w-96 rounded-xl border p-6 text-center focus:text-blue-600">
@@ -35,7 +35,7 @@ return (
     </div>
     
     <div className="mt-4 w-96 rounded-xl border p-6 text-center focus:text-blue-600">
-      <p className="mt-2 text-1xl bottom-1">New to RunAsh? <Link href="/"><a className="text-blue-600">Create an account.</a></Link></p>
+      <p className="mt-2 text-1xl bottom-1">New to RunAsh? <Link href="/"><a className="text-blue-600"> Create an account.</a></Link></p>
     </div>
     </div>
 </main>

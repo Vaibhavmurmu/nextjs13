35e7f9a595644e44dfdc1af2e8ed8d2b99beeaba
Ram Murmu <runashInc@outlook.com>
2022-11-17 14:15:57 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -5,15 +5,15 @@ export default function Live() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-        <div class="md:shrink-0">
-           <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture">
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div className="md:flex">
+        <div className="md:shrink-0">
+           <img className="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture">
         </div>
-         <div class="p-8">
-         <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+         <div className="p-8">
+         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
          <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
-          <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+          <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
         </div>
         </div>
         </div>

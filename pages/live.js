abbfceadb48f237340ca1759f9c463cd284c5dd6
Ram Murmu <runashInc@outlook.com>
2022-11-17 17:29:16 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -1,8 +1,13 @@
-import Link from 'next/link
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Live() {
     return (
-          <div>Live</div>
- 
-  ) 
-}
+        <div className="flex min-h-screen flex-col items-center justify-center py-2">
+            <h1 className="text-6xl font-n">
+                Live
+            </h1>
+        </div>
+        )
+    }

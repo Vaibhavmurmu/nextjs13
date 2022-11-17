6dca6c548e397112afd82637a82c9b4d2ae8ebad
Ram Murmu <runashInc@outlook.com>
2022-11-17 14:20:56 +0530

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -10,7 +10,7 @@ export default function Live() {
         </div>
          <div className="p-8">
          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-         <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
+         <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
           <p className="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
         </div>
         </div>

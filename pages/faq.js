e712b8819f3c87e1030c59408b24d4db4107a234
Ram Murmu <runashInc@outlook.com>
2022-11-19 13:22:21 +0530

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -51,9 +51,9 @@ export default function Faq() {
                         </Link>Go to your dashbord and setup your prefered date and time then displaying your live products contents.
                     </p>
                  </div>
-                    <div className="mt-6 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600 pt-20 max-w-md max-auto px-8 space-y-z">
-                    <details className="text-2xl font-bold p-4 rounded-lg bg-white">
-                    <summary className="text-sm loading-6 font-semibold">If its your first live stream?</summary>
+                    <div className="mt-2 rounded-xl border p-6 text-center hover:text-blue-600 focus:text-blue-600 pt-20 max-w-lg max-auto px-8 space-y-z">
+                    <details className="p-4 rounded-lg bg-white">
+                    <summary className="text-2xl loading-6 font-bold">If its your first live stream?</summary>
                     <p className="text-gray-600 text-sm mt-4 leading-6">
                        Go to your dashbord and edit your stream stting and preview stream 
                     </p>
